77fcfdbebf46aca864edebedd9c738706affbd4f
sovannratana <ouk.sovannratana19@kit.edu.kh>
2022-06-14 01:10:05 +0700

    fetch data from spoonacular api

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,9 +1,21 @@
+import { useEffect } from 'react';
 import React from 'react';
 
 const Popular = () => {
+
+  useEffect(() => {
+    getPopular();
+  },[])
+
+  // fetch data from api
+  const getPopular = async () => {
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+    const data = await api.json();
+    console.log(data);
+  }
+  
   return (
     <div>
-      Popular component
     </div>
   )
 }
